f1199be183a555c0803a4ab0d744081db3641f7a
David Cako <kingcako@gmail.com>
2016-11-18 17:12:45 -0700

    work in progress

--- BootToggle.c
+++ BootToggle.c
@@ -15,16 +15,18 @@ UefiMain (
   )
 {
   EFI_STATUS Status;
-  CHAR16 *VarKey = L"BOOTTOGGLE";
-  void *OutputEnvVar[10];
+  CHAR16 *ToggleKey = L"BOOTTOGGLE";
+  CHAR16 *CountKey = L"BOOTCOUNT";
+  void *ToggleValue[10];
+  void *CountValue[10];
   CHAR16 *TextPath;
   UINTN BufferSize = 0;
   EFI_HANDLE *Buffer = NULL;
   UINTN HandleCounter;
- 
+
   // Get environment variable, testing for L"A" or L"B" value 
-  gRT->GetVariable(VarKey, &gShellVariableGuid, 0, &BuffSize, OutputEnvVar);  
-  switch (OutputEnvVar) { 
+  gRT->GetVariable(ToggleKey, &gShellVariableGuid, 0, &BuffSize, ToggleValue);  
+  switch (ToggleValue) { 
     case L"A":
       *TextPath = L"FS1:\\boot_a.nsh";
       break; 
@@ -33,6 +35,41 @@ UefiMain (
       break;
   }
  
+  // Get count variable, ensuring previous boot was successful
+  gRT->GetVariable(CountKey, &gShellVariableGuid, 0, &BuffSize, CountValue); 
+  switch (CountValue) { 
+    case L"0":
+      switch (ToggleValue) { 
+        case L"A":
+          *TextPath = L"FS1:\\boot_a.nsh";
+          break; 
+        case L"B":
+          *TextPath = L"FS1:\\boot_b.nsh";
+          break;
+      }
+      break; 
+    case L"1": // Previous boot unsuccessful, set to opposite boot toggle value
+      switch (ToggleValue) { 
+          case L"A":
+            gRT->SetVariable(ToggleKey, 
+                             &gShellVariableGuid, 
+                             EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS,
+                             BuffSize, 
+                             L"B");
+            *TextPath = L"FS1:\\boot_b.nsh";
+            break;
+          case L"B":
+            gRT->SetVariable(ToggleKey, 
+                             &gShellVariableGuid, 
+                             EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS,
+                             BuffSize, 
+                             L"A");
+            *TextPath = L"FS1:\\boot_a.nsh";
+            break;
+      }
+      break;
+  }
+
   // Call LocateHandle to get BufferSize
   Status = gBS->LocateHandle(ByProtocol, &gEfiShellProtocolGuid, NULL, &BufferSize, Buffer);  
   
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-#efi-boot-toggle
+#EFI-boot-toggle
 
 EFI program allowing for A/B split booting based on a linux-accessible EFI variable.
+
+Standard use case is OTA-update implementations, where you have this program invoked by startup.nsh, and then have a boot_a.nsh and boot_b.nsh for each image.
+
+EFI variable "BOOTTOGGLE" can be made accessible in Linux through `efivarfs` interface, which you can then set to "A" or "B" to boot from an updated image in-place, while allowing fallback for failed updates.
