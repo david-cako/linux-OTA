25be148a5ca365fc73591cb91bedf91fe31cfc9c
David Cako <kingcako@gmail.com>
2016-12-16 16:09:01 -0700

    add logic, 4 space indentation now...

--- BootToggle.c
+++ BootToggle.c
@@ -10,86 +10,113 @@
 EFI_STATUS
 EFIAPI
 UefiMain (
-  IN EFI_HANDLE        ImageHandle,
-  IN EFI_SYSTEM_TABLE  *SystemTable
-  )
+    IN EFI_HANDLE        ImageHandle,
+    IN EFI_SYSTEM_TABLE  *SystemTable
+    )
 {
-  EFI_STATUS Status;
-  CHAR16 *ToggleKey = L"BOOTTOGGLE";
-  CHAR16 *CountKey = L"BOOTCOUNT";
-  void *ToggleValue[10];
-  void *CountValue[10];
-  CHAR16 *TextPath;
-  CONST CHAR16 *PathA = L"FS1:\\boot_a.nsh";
-  CONST CHAR16 *PathB = L"FS1:\\boot_b.nsh";
-  UINTN BufferSize = 0;
-  EFI_HANDLE *Buffer = NULL;
-  UINTN HandleCounter;
+    EFI_STATUS Status;
+    CHAR16 *UpdateKey = L"UPDATEFLAG";  
+    CHAR16 *ToggleKey = L"BOOTTOGGLE";
+    CHAR16 *CountKey = L"BOOTCOUNT";
+    void *UpdateValue[3]; // these might need to be larger
+    void *ToggleValue[3];
+    void *CountValue[3];
+    CHAR16 *TextPath;
+    CONST CHAR16 *PathA = L"FS1:\\boot_a.nsh";
+    CONST CHAR16 *PathB = L"FS1:\\boot_b.nsh";
+    UINTN BufferSize = 0;
+    EFI_HANDLE *Buffer = NULL;
+    UINTN HandleCounter;
 
-  // Get environment variable, testing for L"A" or L"B" value 
-  gRT->GetVariable(ToggleKey, &gShellVariableGuid, 0, &BuffSize, ToggleValue);  
- 
-  // Get count variable, ensuring previous boot was successful
-  gRT->GetVariable(CountKey, &gShellVariableGuid, 0, &BuffSize, CountValue); 
-  switch (CountValue) { 
-    case L"0":
-      switch (ToggleValue) { 
-        case L"A":
-          TextPath = PathA;
-          break; 
-        case L"B":
-          TextPath = PathB;
-          break;
-      }
-      break; 
-    case L"1": // Previous boot unsuccessful, set to opposite boot toggle value
-      switch (ToggleValue) { 
-          case L"A":
-            gRT->SetVariable(ToggleKey, 
-                             &gShellVariableGuid, 
-                             EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS,
-                             BuffSize, 
-                             L"B");
-            TextPath = PathB;
-            break;
-          case L"B":
-            gRT->SetVariable(ToggleKey, 
-                             &gShellVariableGuid, 
-                             EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS,
-                             BuffSize, 
-                             L"A");
-            TextPath = PathA;
-            break;
-      }
-      break;
-  }
+    // Get toggle variable
+    gRT->GetVariable(ToggleKey, &gShellVariableGuid, 0, 3, ToggleValue);  
+  
+    // Get update flag value
+    gRT->GetVariable(UpdateKey, &gShellVariableGuid, 0, 3, UpdateValue); 
 
-  // Call LocateHandle to get BufferSize
-  Status = gBS->LocateHandle(ByProtocol, &gEfiShellProtocolGuid, NULL, &BufferSize, Buffer);  
+    // test if update flag is enabled
+    switch (UpdateValue) {
+        case L"0":  // no update in process
+            switch (ToggleValue) { 
+                case L"A":
+                    TextPath = PathA;
+                    break; 
+                case L"B":
+                    TextPath = PathB;
+                    break;
+            }
+            break;
+        case L"1":  // update in process
+            // Get count variable, ensuring previous boot wasn't failed
+            gRT->GetVariable(CountKey, &gShellVariableGuid, 0, 3, CountValue); 
+            switch (CountValue) { 
+                case L"0":
+                    switch (ToggleValue) { 
+                        case L"A":
+                            TextPath = PathA;
+                            break; 
+                        case L"B":
+                            TextPath = PathB;
+                            break;
+                        gRT->SetVariable(CountKey, 
+                            &gShellVariableGuid, 
+                            EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS,
+                            3, 
+                            L"1");
+                    }
+                    break; 
+                case L"1": // Previous boot unsuccessful, set to opposite boot toggle value
+                    switch (ToggleValue) { 
+                        case L"A":
+                            gRT->SetVariable(ToggleKey, 
+                                            &gShellVariableGuid, 
+                                            EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS,
+                                            3, 
+                                            L"B");
+                            TextPath = PathB;
+                            break;
+                        case L"B":
+                            gRT->SetVariable(ToggleKey, 
+                                            &gShellVariableGuid, 
+                                            EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS,
+                                            3, 
+                                            L"A");
+                            TextPath = PathA;
+                            break;
+                    }
+                    gRT->SetVariable(UpdateKey, 
+                                    &gShellVariableGuid, 
+                                    EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS,
+                                    3, 
+                                    L"0");
+                    gRT->SetVariable(CountKey, 
+                                    &gShellVariableGuid, 
+                                    EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS,
+                                    3, 
+                                    L"0");
+                    break;
+            }
+
+    // Call LocateHandle to get BufferSize
+    Status = gBS->LocateHandle(ByProtocol, &gEfiShellProtocolGuid, NULL, &BufferSize, Buffer);  
   
-  // AllocateZeroPool using BufferSize
-  Buffer = AllocateZeroPool(BufferSize);
-  Status = gBS->LocateHandle(ByProtocol, &gEfiShellProtocolGuid, NULL, &BufferSize, Buffer);
-  Print(L"locate status: %x\n", Status);
+    // AllocateZeroPool using BufferSize
+    Buffer = AllocateZeroPool(BufferSize);
+    Status = gBS->LocateHandle(ByProtocol, &gEfiShellProtocolGuid, NULL, &BufferSize, Buffer);
+    Print(L"locate status: %x\n", Status);
   
-  // Iterate through handles
-  for (HandleCounter = 0 ; HandleCounter < (BufferSize/sizeof(EFI_HANDLE)) ; HandleCounter++){ 
+    // Iterate through handles
+    for (HandleCounter = 0 ; HandleCounter < (BufferSize/sizeof(EFI_HANDLE)) ; HandleCounter++){ 
     Status = gBS->OpenProtocol(Buffer[HandleCounter],
                                &gEfiShellProtocolGuid,
                                (VOID**)&gEfiShellProtocol,
                                ImageHandle, 
                                NULL,
                                EFI_OPEN_PROTOCOL_GET_PROTOCOL);
-  }
-  
-  gRT->SetVariable(CountKey, 
-                   &gShellVariableGuid, 
-                   EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS,
-                   BuffSize, 
-                   L"1");
+    }
 
-  Status = gEfiShellProtocol->Execute(&ImageHandle, TextPath, NULL, NULL);
-  Print(L"execute status: %p\n", Status);
+    Status = gEfiShellProtocol->Execute(&ImageHandle, TextPath, NULL, NULL);
+    Print(L"execute status: %p\n", Status);
 
-  return EFI_SUCCESS;
+    return EFI_SUCCESS;
 }
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 #EFI-boot-toggle
 
-Still in early stages.
-
 EFI program allowing for A/B split booting based on a linux-accessible EFI variable.
 
 Standard use case is OTA-update implementations, where you have this program invoked by startup.nsh, and then have a boot_a.nsh and boot_b.nsh for each image.
 
 EFI variable "BOOTTOGGLE" can be made accessible in Linux through `efivarfs` interface, which you can then set to "A" or "B" to boot from an updated image in-place, while allowing fallback for failed updates.  An init script is used to set update flag and boot count to zero on successful boot.
+
+Compiles on Linux using EDK.  Couldn't get EDK working on macOS.
 
 #### efi actions:
 - if `UPDATEFLAG` == 0:
